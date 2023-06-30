2b0f002ddcc5ded9508ddcae025b841804c948fa
Bhavesh <diwalebhavesh@gmail.com>
2023-06-30 12:40:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const handleclick = ()=>{
+    document.getElementById('link-group-wrap').style.display="block";
+}
+
+const handlerevclick = ()=>{
+    document.getElementById('link-group-wrap').style.display="none";
+}
